078fb70af0d5641f5f4d79df4cbfc466d9c9c45e
poofy25 <pandamagxd@gmail.com>
2023-03-08 15:21:15 +0000

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
+import { Routes, Route } from "react-router-dom"
 import './App.css'
+
+import HomePage from './pages/homepage';
+import LoginPage from './pages/loginpage';
 
 function App() {
 
   return (
     <div className="App">
-      hi
+      <Routes>
+        <Route path="online-shop/" element={ <HomePage/> } />
+        <Route path="online-shop/login" element={ <LoginPage/> } />
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/homepage.jsx
@@ -0,0 +1,10 @@
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Homepage</h1>
+        </div>
+    )
+}
+
+export default HomePage
