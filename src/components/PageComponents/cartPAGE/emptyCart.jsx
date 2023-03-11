1f97d4df899fbeddee7bc7b9062abeb412850962
poofy25 <pandamagxd@gmail.com>
2023-03-11 08:06:31 +0000

    added emptyCartImage

--- src/components/PageComponents/cartPAGE/emptyCart.jsx
+++ src/components/PageComponents/cartPAGE/emptyCart.jsx
@@ -1,6 +1,6 @@
 import "./emptyCart.css"
 import { useNavigate } from "react-router-dom"
-
+import emptyCartImg from '/src/assets/Images/emptyCartImage.png'
 function EmptyCart() {
     
     const navigate = useNavigate()
@@ -10,6 +10,7 @@ function EmptyCart() {
 
 return (
      <div className="emptyCartContainer">
+        <img src={emptyCartImg}/>
         <p className="emptyCartText">You have no items in cart.</p>
         <p>Dilvin has many articles that are waiting for you!</p>
         <button onClick={goShoppingHandler} className="goShoppingBtn">Go Shopping</button>
