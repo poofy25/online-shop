d402eba3b30122b1af1078257e2c24ab8c57f2cf
poofy25 <pandamagxd@gmail.com>
2023-03-14 19:19:10 +0000

    changed product font size

--- src/pages/homepage/components/HomeWelcome.jsx
+++ src/pages/homepage/components/HomeWelcome.jsx
@@ -2,13 +2,16 @@
 
 import "../styles/homeWelcome.css"
 import welcomeImg from '/src/assets/Images/homePageMobileWelcome.png'
-
+function preload_image(im_url) {
+  const img = new Image();
+  img.src = im_url;
+}
 function HomeWelcome() {
 
 
     return (
         <div className="homeWelcomeContainer">
-          <img src={welcomeImg}/>
+          {preload_image(welcomeImg)}
           <p>Shop the latest trends <br/> with dilVin</p>
           <button>Shop now</button>
         </div>
