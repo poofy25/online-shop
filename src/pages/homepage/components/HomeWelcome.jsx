82338a622c7a4d7eed5ed9b1fb668d07ac3a98a6
poofy25 <pandamagxd@gmail.com>
2023-03-14 20:30:57 +0000

    fixed image

--- src/pages/homepage/components/HomeWelcome.jsx
+++ src/pages/homepage/components/HomeWelcome.jsx
@@ -2,16 +2,13 @@
 
 import "../styles/homeWelcome.css"
 import welcomeImg from '/src/assets/Images/homePageMobileWelcome.png'
-function preload_image(im_url) {
-  const img = new Image();
-  img.src = im_url;
-}
+
 function HomeWelcome() {
 
 
     return (
         <div className="homeWelcomeContainer">
-          {preload_image(welcomeImg)}
+          <img src={welcomeImg}/>
           <p>Shop the latest trends <br/> with dilVin</p>
           <button>Shop now</button>
         </div>
