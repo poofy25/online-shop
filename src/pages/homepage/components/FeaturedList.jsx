d2e31952b1553b26b9e23c8b5ecd01a8414f2fd8
poofy25 <pandamagxd@gmail.com>
2023-03-15 15:28:11 +0000

    added some logic to featured products

--- src/pages/homepage/components/FeaturedList.jsx
+++ src/pages/homepage/components/FeaturedList.jsx
@@ -4,14 +4,60 @@ import "../styles/featuredList.css"
 import Product from "../../../components/product/Product"
 import JsonData from "/data/clothing-data/clothing-data.json"
 
-function FeaturedList() {
+const featuredProductsID = [1,2,3,4,5,6]
 
-console.log(JsonData.products.slice(0,5))
+function FeaturedList() {
+let lastProductItem;
+
     return (
         <div className="featuredContainer">
             <p>Featured right now</p>
             <div className="productList">
-            {JsonData.products.slice(0,5).map
+            {JsonData.products.slice(0,6).map
+            ((item , index) =>{ 
+                  if (index%2==0)
+                  {
+                    lastProductItem = item
+   
+                    return 
+                  }
+
+                  if(index%2!==0){
+
+                    console.log("")
+                    console.log("Idex:"+ index)
+                    console.log("Last Item:"+lastProductItem.id)
+                    console.log("Item:"+item.id)
+                    return(
+                        <div className="twoProductsContainer" key={index + 1}>
+                           
+                            <div className="product" ><Product
+                            product_name={lastProductItem.name}
+                            product_price={lastProductItem.price}
+                            product_img={lastProductItem.image}
+                            /></div>
+                            <div className="product" ><Product
+                            product_name={item.name}
+                            product_price={item.price}
+                            product_img={item.image}
+                            /></div>
+
+                        </div>
+                       )
+
+                  }
+              
+            
+             }
+            )}
+            </div>
+        </div>
+    )
+}
+
+export default FeaturedList
+/*
+ {JsonData.products.slice(0,5).map
             ((item , index) =>{ 
                console.log(item , index)
                return(
@@ -23,13 +69,4 @@ console.log(JsonData.products.slice(0,5))
                )
             }
             )}
-
-
-          
-
-            </div>
-        </div>
-    )
-}
-
-export default FeaturedList
+*/ 
--- src/pages/homepage/components/PromotionsList.jsx
+++ src/pages/homepage/components/PromotionsList.jsx
@@ -13,7 +13,6 @@ function PromotionsList() {
 
             {JsonData.products.slice(0,6).map
             ((item , index) =>{ 
-               console.log(item , index)
                return(
                 <div className="product" key={item.id}><Product
                 product_name={item.name}
