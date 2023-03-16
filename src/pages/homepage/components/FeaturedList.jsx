177f1bfde23c2235f62b7987564dc091facc55ed
poofy25 <pandamagxd@gmail.com>
2023-03-16 18:23:52 +0000

    added searching feature through the json file for product Lists

--- src/pages/homepage/components/FeaturedList.jsx
+++ src/pages/homepage/components/FeaturedList.jsx
@@ -4,19 +4,30 @@ import "../styles/featuredList.css"
 import Product from "../../../components/product/Product"
 import JsonData from "/data/clothing-data/clothing-data.json"
 
-const featuredProductsID = [1,2,3,4,5,6]
+const featuredProductsIDList = [4,1,6,2,3,5];
+const keyValue = "id"
 
 function FeaturedList() {
 let lastProductItem;
+const featuredProductsList = [];
+console.log("1")
+for (let i = 0; i < featuredProductsIDList.length; i++) {
+    for(const productIndex in JsonData.products){
+        if(JsonData.products[productIndex][keyValue] === featuredProductsIDList[i]){
+            featuredProductsList.push(JsonData.products[productIndex])
+        }
+    }
+
+}
+
 
     return (
         <div className="featuredContainer">
             <p>Featured right now</p>
             <div className="productList">
-            {JsonData.products.slice(0,6).map
+            {featuredProductsList.map
             ((item , index) =>{ 
-                  if (index%2==0)
-                  {
+                  if (index%2==0){
                     lastProductItem = item
    
                     return 
@@ -24,10 +35,6 @@ let lastProductItem;
 
                   if(index%2!==0){
 
-                    console.log("")
-                    console.log("Idex:"+ index)
-                    console.log("Last Item:"+lastProductItem.id)
-                    console.log("Item:"+item.id)
                     return(
                         <div className="twoProductsContainer" key={index + 1}>
                            
--- src/pages/homepage/components/PromotionsList.jsx
+++ src/pages/homepage/components/PromotionsList.jsx
@@ -3,7 +3,25 @@
 import "../styles/promotionsList.css"
 import Product from "../../../components/product/Product"
 import JsonData from "/data/clothing-data/clothing-data.json"
+
+const promotionProductsIDList = [2,5,3,6,4,1];
+const keyValue = "id"
+
+
+
 function PromotionsList() {
+    let lastProductItem;
+    const promotionProductsList = [];
+
+    for (let i = 0; i < promotionProductsIDList.length; i++) {
+        for(const productIndex in JsonData.products){
+            if(JsonData.products[productIndex][keyValue] === promotionProductsIDList[i]){
+                promotionProductsList.push(JsonData.products[productIndex])
+            }
+        }
+    
+    }
+
 
 
     return (
@@ -11,6 +29,52 @@ function PromotionsList() {
             <p>Promotions</p>
             <div className="promotionProductList">
 
+            {promotionProductsList.map
+            ((item , index) =>{ 
+                  if (index%2==0){
+                    lastProductItem = item
+   
+                    return 
+                  }
+
+                  if(index%2!==0){
+
+                    return(
+                        <div className="twoProductsContainer" key={index + 1}>
+                           
+                            <div className="product" ><Product
+                            product_name={lastProductItem.name}
+                            product_price={lastProductItem.price}
+                            product_img={lastProductItem.image}
+                            /></div>
+                            <div className="product" ><Product
+                            product_name={item.name}
+                            product_price={item.price}
+                            product_img={item.image}
+                            /></div>
+
+                        </div>
+                       )
+
+                  }
+              
+            
+             }
+            )}
+
+
+
+            </div>
+        </div>
+    )
+}
+
+export default PromotionsList
+
+
+
+
+/*
             {JsonData.products.slice(0,6).map
             ((item , index) =>{ 
                return(
@@ -21,11 +85,4 @@ function PromotionsList() {
                 /></div>
                )
             }
-            )}
-
-            </div>
-        </div>
-    )
-}
-
-export default PromotionsList
+            )}*/
