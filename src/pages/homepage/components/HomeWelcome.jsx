7c58f0bbec343ea0aa492fb5c8470c91a2d54b0b
poofy25 <pandamagxd@gmail.com>
2023-03-19 10:48:12 +0000

    fixed error on build

--- src/pages/homepage/components/HomeWelcome.jsx
+++ src/pages/homepage/components/HomeWelcome.jsx
@@ -2,16 +2,15 @@
 
 import "../styles/homeWelcome.css"
 import welcomeImg from '/src/assets/Images/homePageMobileWelcome.png'
-import PreloadImage from 'react-preload-image'
+
 
 function HomeWelcome() {
 
 
     return (
         <div className="homeWelcomeContainer">
-          <PreloadImage
+          <img
           src={welcomeImg}
-          lazy
           className="homeIMG"
           />
           <p>Shop the latest trends <br/> with dilVin</p>
