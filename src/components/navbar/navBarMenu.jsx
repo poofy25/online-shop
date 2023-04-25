305eed063373c88ff40efa5fdf5203d83304d72e
poofy25 <pandamagxd@gmail.com>
2023-04-25 17:30:20 +0100

    stlyed nav menu

--- src/components/navbar/navBarMenu.jsx
+++ src/components/navbar/navBarMenu.jsx
@@ -1,6 +1,11 @@
 
 import './navBarMenu.css'
 import { useNavigate } from 'react-router-dom'
+import logedOutIcon from '/src/assets/Icons/logedOutProfileIcon.png'
+import cartIcon from '/src/assets/Icons/cartIcon.png'
+import contactUsIcon from '/src/assets/Icons/contactUsIcon.png'
+import shoppingWoman from '/src/assets/Clothing/womanShopping.png'
+import womanLooking from '/src/assets/Clothing/womanLooking.png'
 
 const NavBarMenu = (props)=>{
     const menuBtnHandler = props.closeMenu
@@ -10,16 +15,16 @@ const NavBarMenu = (props)=>{
     <div className="menuContainer">
       <div className="menuContainerShadow" onClick={menuBtnHandler}></div>
       <div className="menuContainerContent">
-        <button className="menuHomeBtn menuBigBtn" onClick={()=>navigateTo('/')}>HOME</button>
-        <button className="menuCatalogBtn menuBigBtn" onClick={()=>navigateTo('/catalog')}>CATALOG</button>
+        <button className="menuHomeBtn menuBigBtn" onClick={()=>navigateTo('/')}>HOME <img src={womanLooking}/></button>
+        <button className="menuCatalogBtn menuBigBtn" onClick={()=>navigateTo('/catalog')}>CATALOG <img src={shoppingWoman}/></button>
 
 
         <div className="menuSignInContainer">
           <a onClick={()=>navigateTo('/login')}>Sign In</a>&nbsp; | &nbsp;<a onClick={()=>navigateTo('/login')}>SignUp</a>
         </div>
-        <button className="menuHomeBtn menuSmallBtn" onClick={()=>navigateTo('/account')}>Account</button>
-        <button className="menuCartBtn menuSmallBtn">Cart</button>
-        <button className="menuContactBtn menuSmallBtn">Contact Us</button>
+        <button className="menuHomeBtn menuSmallBtn" onClick={()=>navigateTo('/account')}><img src={logedOutIcon}/>Account</button>
+        <button className="menuCartBtn menuSmallBtn"><img src={cartIcon}/>Cart</button>
+        <button className="menuContactBtn menuSmallBtn"><img src={contactUsIcon}/>Contact Us</button>
         <div className="menuCompanyInfoBtn">About dilVin</div>
         <div className="menuSocialsContainer">Insta , Facebook , Snap</div>
       </div>
