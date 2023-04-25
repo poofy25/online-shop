3d846d8a9f9532cf7b2c225c7f71b3208bb93ac9
poofy25 <pandamagxd@gmail.com>
2023-04-25 17:49:13 +0100

    updated nav menu styles

--- src/components/navbar/navBarMenu.jsx
+++ src/components/navbar/navBarMenu.jsx
@@ -1,20 +1,32 @@
 
 import './navBarMenu.css'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate , useLocation} from 'react-router-dom'
+import { useEffect } from 'react'
 import logedOutIcon from '/src/assets/Icons/logedOutProfileIcon.png'
 import cartIcon from '/src/assets/Icons/cartIcon.png'
 import contactUsIcon from '/src/assets/Icons/contactUsIcon.png'
 import shoppingWoman from '/src/assets/Clothing/womanShopping.png'
 import womanLooking from '/src/assets/Clothing/womanLooking.png'
+import companyLogo from '/src/assets/Icons/companyLogo.png'
 
 const NavBarMenu = (props)=>{
     const menuBtnHandler = props.closeMenu
     const navigateTo = useNavigate()
+    const location = useLocation()
+
+    useEffect(()=>{
+      
+      if(document.querySelector('.menuContainer.open')){
+        console.log('changed locations')
+      menuBtnHandler()
+      }
+    },[location])
 
     return(
     <div className="menuContainer">
       <div className="menuContainerShadow" onClick={menuBtnHandler}></div>
       <div className="menuContainerContent">
+        <img className='menuCompanyLogo' src={companyLogo}/>
         <button className="menuHomeBtn menuBigBtn" onClick={()=>navigateTo('/')}>HOME <img src={womanLooking}/></button>
         <button className="menuCatalogBtn menuBigBtn" onClick={()=>navigateTo('/catalog')}>CATALOG <img src={shoppingWoman}/></button>
 
@@ -23,7 +35,7 @@ const NavBarMenu = (props)=>{
           <a onClick={()=>navigateTo('/login')}>Sign In</a>&nbsp; | &nbsp;<a onClick={()=>navigateTo('/login')}>SignUp</a>
         </div>
         <button className="menuHomeBtn menuSmallBtn" onClick={()=>navigateTo('/account')}><img src={logedOutIcon}/>Account</button>
-        <button className="menuCartBtn menuSmallBtn"><img src={cartIcon}/>Cart</button>
+        <button className="menuCartBtn menuSmallBtn" onClick={()=>navigateTo('/cart')}><img src={cartIcon}/>Cart</button>
         <button className="menuContactBtn menuSmallBtn"><img src={contactUsIcon}/>Contact Us</button>
         <div className="menuCompanyInfoBtn">About dilVin</div>
         <div className="menuSocialsContainer">Insta , Facebook , Snap</div>
